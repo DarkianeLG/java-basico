f8ef3c88cdd7ae8d0e5d58d783fffb123b277f23
Darkiane Lima <darkianelima13@gmail.com>
2024-05-16 21:15:23 -0300

    Criação do arquivo for em array

---
+++ java-basico/src/ForArray.java
@@ -0,0 +1,13 @@
+public class  ForArray{
+
+    public static void main (String[] args){
+        String alunos [] =  {"FELIPE", "PAULO", "ANDRE", "MARCELA"};
+
+        //Forma abreviada
+
+        for(String aluno : alunos){
+            System.out.println(aluno);
+        }
+    }
+
+}
